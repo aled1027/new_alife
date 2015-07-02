72877121194d1016f34876227d182ed1b91c3239
Jacob <jmenick@reed.edu>
2015-07-02 16:28:09 -0700

    almost done porting parallelMap. Need to figure out shared state

--- alife/util/dbutil.py
+++ alife/util/dbutil.py
@@ -1,9 +1,17 @@
 # Utilities for reading data from mongodb. 
+
+# TODO - in the testing environment, need to find a way to share the collection accross processes. 
+
+import random
+import logging
+import multiprocessing as mp
 from collections import defaultdict
+import pymongo
+import mongomock as mm
 from pymongo import MongoClient
+from pymongo.errors import BulkWriteError
 from alife import mockdb
 from alife.util.general import save_dict
-
 
 _db = MongoClient().patents
 
@@ -26,7 +34,7 @@ def get_texts(patents, coll_name = 'pat_text', generator = False):
         return list(texts)
 
 
-def crawl_lineage(ancestor_pno, n_generations=3,fields = ['_id', 'citedby'], 
+def crawl_lineage(ancestor_pno, db, n_generations=3,fields = ['_id', 'citedby'], 
                   enforce_func = lambda pat: len(pat.get('citedby', [])) > 75):
     """
     Get all patents children of ancestor_pno, and children's children..., 
@@ -34,14 +42,14 @@ def crawl_lineage(ancestor_pno, n_generations=3,fields = ['_id', 'citedby'],
     Returns a list of lists of patent documents, where each inner list
     contains all patents in a given generation satisfying enforce_func. 
     """
-    ancestor_doc = _db.cite_net.find_one({'_id': ancestor_pno}, {field:1 for field in fields})
+    ancestor_doc = db.cite_net.find_one({'_id': ancestor_pno}, {field:1 for field in fields})
     lineage = [[ancestor_doc]]
     for i in range(1,n_generations):
         ancestors = [pat for pat in lineage[i-1]]
         descendants = []
         for a in ancestors:
             for child_pno in a['citedby']:
-                child_doc = _db.cite_net.find_one({'_id': int(child_pno)}, {field: 1 for field in fields})
+                child_doc = db.cite_net.find_one({'_id': int(child_pno)}, {field: 1 for field in fields})
                 if child_doc is None:
                     continue
                 else:
@@ -66,3 +74,90 @@ def subnet_adj_dict(patents):
             if citer_pno in pnos:
                 incites[citee_pno].append(int(citer_pno))
     return incites
+
+
+def parallelMap(func, collection, id_field_name, enforce_func = None,
+                updateFreq=50, verbose=True):
+                """
+                Refactored Drew Blount's Parallel map to make it testable in a mockdb environment.
+                This takes as input a function and collection, and maps the function
+                over the whole collection. It is intended for particular kinds of functions:
+                those which update a field or set of fields for a single document. 
+                So func is required to take as input a single document and 
+                return a dictionary which may be passed as the argument to a batch.update() method.
+                E.g of the form 
+                
+                    {'$set': {'my_field1': my_value, 'myfield2': value}}
+                
+                
+                The findArgs parameter allows the user to subset the collection by only 
+                mapping the function over documents which match a query given in findArgs[spec]. 
+                findArgs[fields] is a projection, determining which fields are in play for the function.
+
+                the updateFreq parameter determines how often the bulk operation is executed. 
+
+                This version of the code does not bother with cursor batches. It will be interesting 
+                to see a benchmark.
+                """
+                if enforce_func is None:
+                    enforce_func = lambda x: True
+
+                def partFunc(cursor, collection):
+                    if type(collection) == pymongo.collection.Collection:
+                        """
+                        If the collection is a real pymongo collection,
+                        operate as usual with bulk ops. 
+                        """
+                        bulk = collection.initialize_unordered_bulk_op()
+                        updateNum = 0
+                        anyToAdd = False
+                        for doc in cursor:
+                            if not enforce_func(doc):
+                                continue
+                            updateNum += 1
+                            out = func(doc)
+                            if out:
+                                bulk.find({id_field_name: doc[id_field_name]}).update_one(out)
+                                anyToAdd = True
+                            if updateNum == updateFreq:
+                                if anyToAdd:
+                                    bulk.execute()
+                                    bulk = collection.initialize_unordered_bulk_op()
+                                    anyToAdd = False
+                                updateNum = 0
+                        if anyToAdd:
+                            bulk.execute()
+                    elif type(collection) == mm.collection.Collection:
+                        for doc in cursor:
+                            if not enforce_func(doc):
+                                continue
+                            if verbose == True:
+                                print "enforce func holds for patent {}".format(doc[id_field_name])
+                            res = collection.update({id_field_name: doc[id_field_name]},func(doc))
+                            if verbose:
+                                print res
+                    else:
+                        raise RuntimeError("collection type is not pymongo or mongomock??")
+
+                cursors = collection.parallel_scan(mp.cpu_count())
+                workerProcesses = []
+                for c in cursors:
+                    p = mp.Process(target=partFunc, args=(c,))
+                    p.daemon = True
+                    p.start()
+                    workerProcesses.append(p)
+                    p.join()
+
+                for p in workerProcesses:
+                    p.join()
+
+                return collection.find({'dummy_field': 'yay!'}).count()
+    
+def test():
+    patns = mockdb.get_mock().patns
+    def test_func(doc):
+        return {'$set': {'dummy_field': 'yay!'}}
+    n_right = parallelMap(test_func, patns, 'pno')
+    print "num for which alleged successfull: {}".format(n_right)
+    print "num with dummy_field: {}".format(patns.find({'dummy_field': 'yay!'}).count())
+    return patns
--- tests/test_util/test_dbutil.py
+++ tests/test_util/test_dbutil.py
@@ -1,13 +1,18 @@
 #tests for dbutil. 
 import unittest
+from alife.mockdb import get_mock
 from alife.util import dbutil
-from pymongo import MongoClient
 
-#global
-DB = MongoClient().patents
-
-class TestGetText(unittest.TestCase):
-    pass
+class TestParallelMap(unittest.TestCase):
+    def setUp(self):
+        self.mockdb = get_mock()
+    
+    def testAppliesFuncToAllDocs(self):
+        enforce_func = lambda x: 'pno' in x
+        def test_func(doc):
+            return {'$set': {'dummy_field': 'yay!'}}
+        dbutil.parallelMap(test_func, self.mockdb.patns, 'pno', enforce_func)
+        self.assertEqual(self.mockdb.patns.find({'dummy_field': 'yay!'}).count(), 1000)
 
 if __name__ == '__main__':
     unittest.main()
