2bba6a2f9c0f109f7d4cc221c53a6d9d934ac2e3
Jacob <jmenick@reed.edu>
2015-07-06 13:12:26 -0700

    refactored reach funcs, ready to go

--- alife/dooropening/reach.py
+++ alife/dooropening/reach.py
@@ -1,18 +1,15 @@
 # measure 'door-openingness' in patents by a few different ways of measuring reach. 
 from pymongo import MongoClient
 from itertools import product
-from mockdb import get_mock
+from alife.mockdb import get_mock
 from alife.util.dbutil import crawl_lineage
 from alife.txtmine.w2v import _dist as nrml_cos_sim
-from jmAlife.dbManage.parallelMap import partallelMap
+from jmAlife.dbManage.parallelMap import parallelMap
 import logging
+import numpy as np
 
 _realdb = MongoClient().patents
 _mockdb = get_mock()
-_trait_info = {
-        'tf-idf': ('top_tf-idf', tfidf_dist),
-        'w2v': ('doc_vec', w2v_dist)
-    }
 
 def tfidf_dist(traits_a, traits_b):
     """
@@ -38,6 +35,11 @@ def w2v_dist(traits_a, traits_b):
     assert(traits_a.shape[0] == traits_b.shape[0])
     return 1 - nrml_cos_sim(traits_a, traits_b)
 
+_trait_info = {
+        'tf-idf': ('top_tf-idf', tfidf_dist),
+        'w2v': ('doc_vec', w2v_dist)
+    }
+
 """ # JM comment out. Development of this function suspended as it is infeasible. 
 def all_pairs_trait_distance(ancestor_pno, trait='w2v', db = _realdb, n_generations = 5):
     #
@@ -56,23 +58,48 @@ def all_pairs_trait_distance(ancestor_pno, trait='w2v', db = _realdb, n_generati
     lineage = [doc.get(trait_field, None) for doc in lineage]
 """
 
-def trait_variance(ancestor_pno, trait='w2v', db = _realdb, n_gens = 5):
+def trait_variance(ancestor_pno, trait='w2v', db = _realdb, n_gens = 2):
     """
-    Computes the trait mean and variance. 
+    Computes the trait mean and variance*. For now the variance is the norm of the vector of component-wise variances.  
     """
     stats = {}
-    trait_field,dist_func = _trait_info[trait]
-    parent = db.traits.find_one({'_id': parent_pno}, {'_id': 1, 'citedby': 1, trait_field:1})
+    mean_field_name = str(n_gens)+'_gen_trait_mean_' + trait
+    var_field_name = str(n_gens)+'_gen_trait_variance_' + trait
+    trait_field, _ = _trait_info[trait]
+    parent = db.traits.find_one({'_id': ancestor_pno}, {'_id': 1, 'citedby': 1, trait_field:1})
     lineage = crawl_lineage(ancestor_pno, n_gens, fields = ['_id', 'citedby', trait_field], flatten=True,
                             enforce_func = lambda x: True)
     traits = [doc.get(trait_field, None) for doc in lineage]
-    traits = np.array([trait for trait in traits if trait is not None])
-    mean_field_name = str(n_gens)+'_gen_trait_mean_' + trait
-    var_field_name = str(n_gens)+'_gen_trait_variance_' + trait
-    stats[mean_field_name] = np.mean(traits, axis=0)
-    stats[var_field_name] = np.var(traits, axis=0)
+    traits = np.array([t for t in traits if t is not None], dtype=np.float64)
+    stats[mean_field_name] = list(np.mean(traits, axis=0))
+    stats[var_field_name] = np.linalg.norm(np.var(traits, axis=0)) # For now, get a real number by computing norm. 
     return stats
 
+def parent_child_trait_distance(ancestor_pno, trait='w2v', db=_realdb, n_gens = 2):
+    stats = {}
+    sum_fieldname = '_'.join([str(n_gens), 'gen_sum_dist', trait])
+    avg_fieldname = '_'.join([str(n_gens), 'gen_avg_dist', trait])
+    trait_field,dist_func = _trait_info[trait]
+    parent = db.traits.find_one({'_id': ancestor_pno}, {'_id': 1, 'citedby': 1, trait_field:1})
+    lineage = crawl_lineage(ancestor_pno, n_gens, fields = ['_id', 'citedby', trait_field], flatten=True,
+                            enforce_func = lambda x: True)[1:]
+    if not (parent.get('citedby', None) and parent.get(trait_field, None)):
+        stats[sum_fieldname] = 0
+        stats[avg_fieldname] = 0
+        return stats
+    traits = [doc.get(trait_field, None) for doc in lineage]
+    traits = np.array([t for t in traits if t is not None], dtype=np.float64)
+    n_children_with_traits = len(traits)
+    if n_children_with_traits == 0:
+        stats[sum_fieldname] = -1
+        stats[avg_fieldname] = -1
+        return stats
+    dist_sum = np.sum([dist_func(parent.get(trait_field), trait) for trait in traits])
+    stats[sum_fieldname] = dist_sum
+    stats[avg_fieldname] = dist_sum/n_children_with_traits
+    return stats
+    
+"""
 def first_order_trait_distance(parent_pno, trait='w2v', db =_realdb):
     """
     Compute the sum and average pairwise trait distance between
@@ -110,7 +137,8 @@ def first_order_trait_distance(parent_pno, trait='w2v', db =_realdb):
         return stats
     stats[avg_fieldname] = float(door_openingness)/n_children_with_traits 
     return stats
-
+"""
+"""
 def compute_tfidf_do():
     def one_tfidf_do(doc):
         return {'$set': first_order_trait_distance(doc['_id'], 'tf-idf')}
@@ -128,9 +156,40 @@ def compute_tfidf_do():
                 },
                 updateFreq = 500,
                 bSize = 1000)
+"""
 
-def test():
-    p = _mockdb.traits.find().next()
-    fotd = first_order_trait_distance(p['_id'])
-    mv = trait_variance(p['id'])
-    return fotd,mv,p
+def compute_reach(trait='w2v', n_gens=1):
+    trait_field, _ = trait_info[trait]
+    def one_reach(doc):
+        return {'$set': parent_child_trait_distance(doc['_id'], n_gens=n_gens, trait=trait)}
+    parallelMap(
+        one_reach,
+        in_collection = _realdb.traits,
+        out_collection = _realdb.traits,
+        findArgs = {
+            'spec': {trait_field: {'$exists': True}},
+            'fields': {trait_field: 1, 'citedby': 1, '_id': 1}
+        },
+        updateFreq=500,
+        bSize = 1000
+    )
+
+def compute_w2v_trait_variance(trait='w2v', n_gens=1):
+    trait_field, _ = trait_info[trait]
+    def one_trait_var(doc):
+        return {'$set': trait_variance(doc['_id'], n_gens=n_gens, trait = trait)}
+    parallelMap(
+        one_trait_var,
+        in_collection = _realdb.traits,
+        out_collection = _realdb.traits,
+        findArgs = {
+            'spec': {trait_field: {'$exists': True}},
+            'fields': {trait_field: 1, 'citedby': 1, '_id': 1}
+        },
+        updateFreq=500,
+        bSize = 1000
+    )
+    
+
+def main():
+    compute_reach()
