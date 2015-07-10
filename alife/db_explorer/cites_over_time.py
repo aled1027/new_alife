030564ef1a21801d09013cb6e8d9cba4870ac728
Jacob <jmenick@reed.edu>
2015-07-10 12:04:19 -0700

    fixed cites_over_time so that we aren't missing so many isds

--- alife/db_explorer/cites_over_time.py
+++ alife/db_explorer/cites_over_time.py
@@ -2,76 +2,58 @@
 
 from alife.mockdb import get_mock
 from alife.util.general import save_dict
+from alife.util.dbutil import get_fields_unordered
 from pymongo import MongoClient
 from collections import Counter
 import datetime
 import matplotlib.pyplot as plt
-from matplotlib.dates import date2num
-import numpy as np
 
 _sentinel_date = datetime.datetime(year=1970,month=1,day=1)
 
-def in_cites_over_time(db, limit=100):
+def cites_over_time(db, limit=100):
     """
-    Returns a list of dates, one for each time a citation of a patent 
-    issued on that date occurs. 
+    Returns an incites and outcites counter, a dictionary
+    containing the counts of incites,outcites at each date.
     """
-    if limit is not None:
-        cites = db.just_cites.find().limit(limit)
-    else:
-        cites = db.just_cites.find()
-    return np.array(
-        map(date2num, 
-            [cite.get('cited_date', _sentinel_date) for cite in cites]
-        )
+    isds,incites,outcites = get_fields_unordered(
+        db.patns, 
+        ['isd', 'citedby', 'rawcites'], 
+        [_sentinel_date, [], []],
+        limit
     )
-
-def out_cites_over_time(db, limit=100):
-    """
-    Return a list of dates, one for each outgoing citation on that date.
-    """
-    if limit is not None:
-        cites = db.just_cites.find().limit(limit)
-    else:
-        cites = db.just_cites.find()
-    return np.array(
-        map(date2num, 
-            [cite.get('citer_date', _sentinel_date) for cite in cites]
-        )
-    )
+    incite_ctr = Counter()
+    outcite_ctr = Counter()
+    incite_ctr.update({date:len(cites) for date,cites in zip(isds, incites)})
+    outcite_ctr.update({date: len(cites) for date,cites in zip(isds, outcites)})
+    return incite_ctr, outcite_ctr
 
 def test():
     db = get_mock()
-    in_dates = in_cites_over_time(db)
-    out_dates = out_cites_over_time(db)
+    inctr, outctr = cites_over_time(db,limit=100)
+    in_dates, in_cites = zip(*inctr.items())
+    out_dates, out_cites = zip(*outctr.items())
     f,(ax1, ax2) = plt.subplots(1,2,sharey=True)
     f.set_size_inches(18.5, 10.5)
-    ax1.hist(in_dates)
+    ax1.scatter(in_dates, in_cites)
     ax1.set_xlabel('Date')
     ax1.set_title('In-degrees over Time.')
-    ax2.hist(out_dates)
+    ax2.scatter(out_dates, out_cites)
     ax2.set_xlabel('Date')
     ax2.set_title('Out-Degrees over Time.')
-    return in_dates, out_dates
+    plt.show()
+    return inctr, outctr
     
 def main():
-    ctr_indates = Counter()
-    ctr_outdates = Counter()
     db = MongoClient().patents
-    in_dates = in_cites_over_time(db, limit = None)
-    out_dates = out_cites_over_time(db, limit = None)
-    ctr_indates.update(in_dates)
-    ctr_outdates.update(out_dates)
-    incites_fn = 'incites_over_time_hist.p'
-    outcites_fn = 'outcites_over_time_hist.p'
-    save_dict(incites_fn, dict(ctr_indates))
-    save_dict(outcites_fn, dict(ctr_outdates))
+    inctr, outctr = cites_over_time(db,limit=None)
+    in_dates, in_cites = zip(*inctr.items())
+    out_dates, out_cites = zip(*outctr.items())
     f,(ax1, ax2) = plt.subplots(1,2,sharey=True)
     f.set_size_inches(18.5, 10.5)
-    ax1.hist(in_dates, bins=100)
+    ax1.scatter(in_dates, in_cites)
     ax1.set_xlabel('Date')
     ax1.set_title('In-degrees over Time.')
-    ax2.hist(out_dates, bins=100)
+    ax2.scatter(out_dates, out_cites)
     ax2.set_xlabel('Date')
     ax2.set_title('Out-Degrees over Time.')
     plt.savefig('cites_over_time.png')
