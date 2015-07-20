b84d8044624fd71c3af7890599bac8666abfcdd7
Jacob <jmenick@reed.edu>
2015-07-19 20:51:58 -0700

    now the root init file has the family and friends patent numbers for convenience

---
+++ alife/__init__.py
@@ -0,0 +1,13 @@
+# Include the patent numbers for the subset of patents with which we have
+#  been doing microanalysis
+_allstar_pnos = [
+    4655771, 4061724, 4723129, 5103459, 4683202, 
+    5143854, 4064521, 4340563, 4405829, 5572643]
+_small_friend_pnos = [
+    6000704, 4766623, 5454348, 4880023, 5191766, 
+    3938459, 6377014, 5284351, 4417707, 6004032]
+_big_friend_pnos = [
+    5616141, 4287607, 4119312, 4667431, 4154143, 
+    4274976, 5491084, 4061199, 4725057, 4945360]
+_normal_pnos = _small_friend_pnos + _big_friend_pnos
+_microanalysis_pnos = _allstar_pnos + _small_friend_pnos + _big_friend_pnos
