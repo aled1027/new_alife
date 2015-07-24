0684f48e2f66683b99e4175d4d79faab6545ce67
Jacob <jmenick@reed.edu>
2015-07-24 13:43:56 -0700

    wrote code for inserting lda topics into the db.

--- alife/pipeline/incidentals/insert_lda_topics.py
+++ alife/pipeline/incidentals/insert_lda_topics.py
@@ -1,42 +1,53 @@
-# Inserts a field for every document for their LDA topics (in decreasing order of strength)
+# Inserts a field for every document for their LDA topic indices and strengths (in decreasing order of strength)
 from pymongo import MongoClient
-from alife.txtmine.lda import load_lda
+from alife.txtmine.lda import load_lda, load_vocab, load_corpus
+from alife.util.general import load_obj
 from pprint import pprint
 from jmAlife.dbManage.parallelMap import parallelMap
 
 def test(limit):
-    # db = MongoClient().patents
-    # lda = load_lda(lda_fn)
-    # pnos = load_pno2id(pno2id_fn)
-    # corpus = load_corpus(corpusfn)
-    # doc_topics = [lda[doc] for doc in corpus]
-    # def partfunc(doc):
-    #    return {'$set': {'lda_topics': doc_topics[pno2id[doc['_id']]]}}
-    # pats_test = db.traits.find().limit(limit)
-    # for p in pats_test:
-    #   pprint(partfunc(p))
-    pass
+    indir = '/Users/jmenick/Desktop/alife_refactor/output/lda_model_200'
+    name = 'lda_200'
+    ldafn = '/'.join([indir, name+'.lda'])
+    corpusfn = '/'.join([indir, 'corpus_' + name + '.svmlight'])
+    pnofn = '/'.join([indir, 'pnos.p'])
+    corpus = load_corpus(corpusfn)
+    pno2id = {pno:i for i,pno in enumerate(load_obj(pnofn))}
+    lda = load_lda(ldafn)
+    assert(len(pno2id) == len(corpus))
+    def partfunc(doc):
+        doc_topics = lda[corpus[pno2id[doc['_id']]]]
+        return {'$set': {'lda_topics': doc_topics}}
+    db = MongoClient().patents
+    test_patns = db.pat_text.find().limit(limit)
+    for patn in test_patns:
+        pprint(partfunc(patn))
 
 def main():
-    # db = MongoClient().patents
-    # lda = load_lda(lda_fn)
-    # pnos = load_pno2id(pno2id_fn)
-    # corpus = load_corpus(corpusfn)
-    # doc_topics = [lda[doc] for doc in corpus]
-    # def partfunc(doc):
-    #    return {'$set': {'lda_topics': doc_topics[pno2id[doc['_id']]]}}
-    # parallelMap(
-    #   partfunc,
-    #   in_collection = db.traits,
-    #   out_collection = db.traits,
-    #   findArgs = {
-    #     'spec': {},
-    #     'fields': {},
-    #   },
-    #   updateFreq = 500,
-    #   bSize = 1000
-    #)
-    pass
+    indir = '/Users/jmenick/Desktop/alife_refactor/output/lda_model_200'
+    name = 'lda_200'
+    ldafn = '/'.join([indir, name+'.lda'])
+    corpusfn = '/'.join([indir, 'corpus_' + name + '.svmlight'])
+    pnofn = '/'.join([indir, 'pnos.p'])
+    corpus = load_corpus(corpusfn)
+    pno2id = {pno:i for i,pno in enumerate(load_obj(pnofn))}
+    lda = load_lda(ldafn)
+    assert(len(pno2id) == len(corpus))
+    def partfunc(doc):
+        doc_topics = lda[corpus[pno2id[doc['_id']]]]
+        return {'$set': {'lda_topics': doc_topics}}
+     db = MongoClient().patents
+     parallelMap(
+         partfunc,
+         in_collection = db.traits,
+         out_collection = db.traits,
+         findArgs = {
+             'spec': {},
+             'fields': {},
+         },
+         updateFreq = 500,
+         bSize = 1000
+     )
 
 if __name__ == '__main__':
     test()
